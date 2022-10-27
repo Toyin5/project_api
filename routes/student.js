ff02287ca940a99201f366ff0a394cf2ec5aeeca
toyin <toyinmuhammed50@gmail.com>
2022-10-27 08:56:48 +0200

    Edited student route

--- routes/student.js
+++ routes/student.js
@@ -3,5 +3,5 @@ import { registerStudent, getStudents } from "../controllers/student.js";
 
 export const studentRouter = express.Router();
 
-studentRouter.post("/register/student", registerStudent);
+studentRouter.post("/register/student/:code", registerStudent);
 studentRouter.get("/get/students/:code", getStudents);
